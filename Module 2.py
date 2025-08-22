99c768ab5f95c97982b32e9d9c18323fffc3568f
amberward423 <amberward423@gmail.com>
2025-08-22 15:31:17 +0300

    Third commit

---
+++ Module 2.py
@@ -0,0 +1,29 @@
+#user = input("Enter your name: ")
+#print("Nice to meet you,"+user+"")
+#print (type(user))
+#list1= [1,2,3]
+#print(list1)
+#tup1=(1,2,3)
+#print(type(tup1))
+#a=input("Enter a number: ")
+#ca=int(a)
+#b=input("Enter another number: ")
+#cb=int(b)
+#c=ca/cb
+#print(c)
+#print(type(a))
+#print(type(ca))
+#print(type(b))
+#print(type(cb))
+#print(type(c))
+user = input("Enter your math score:")
+user = input("Enter your physics score:")
+user = input("Enter your programming score:")
+sum1 = int(user) + int(user) +int(user)
+print("Total score:" +sum1+"")
+sum2 = (sum1*100)/(300)
+print("Your score is:" +sum2+"")
+
+
+
+
