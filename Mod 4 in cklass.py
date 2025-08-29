31eda658c076a7a3fd792c729543469789c1609e
amberward423 <amberward423@gmail.com>
2025-08-29 14:53:19 +0300

    Fourth commit

---
+++ Mod 4 in cklass.py
@@ -0,0 +1,50 @@
+rounds=int(input("How many greetings:"))
+
+finished_rounds=0
+
+while finished_rounds<rounds:
+    finished_rounds=finished_rounds+1
+
+command= input("Enter command:")
+
+while command!= "stop":
+    command=input("Enter command: ")
+    print("We are free, what should we do, let's " +command)#
+
+print("Execution stopped explicitly.")
+
+import random
+a= random.randint(1,6)
+b=random.randint(1, 9)
+print (a)
+
+import random
+dice1=dice2=rolls=0
+
+while (dice1!=6 or dice2!=6):
+    dice1=random.randint(1,6)
+    dice2=random.randint(1,6)
+    rolls=rolls+1
+print(f"Rolled {rolls:d} times.")
+
+
+first=1
+while first <=5:
+   second=1
+   while second <=5:
+        print(f"{first}times{second} is {first*second:d}")
+        second=second+1
+    first=first+1
+
+first=2
+while first <=2:
+    second=1
+    while second <=10:
+        print(f"{first} times {second} is {first*second:d}")
+        second=second+1
+    first=first+1
+
+
+rounds= rounds + 1
+total_rolls= total_rolls + rolls
+average_rolls= total_rolls / rounds
---
+++ Mod3 excercise.py
@@ -0,0 +1,28 @@
+#1
+user_1= int(input("What is the length of the zander you've caught?"))
+zander=42-user_1
+
+if user_1 <42:
+    print("Please release the zander, it is too small. The zander is ", +zander ,"cm below the accepted limit of 42 cm " )
+else:
+    print ("Enjoy your catch!")
+#2
+
+
+
+user_2= input("What is the cabin class of your cabin")
+
+if user_2==a:
+    print("Your cabin is an ",{a}, "it has a window")
+
+elif user_2 == b:
+    print("Your cabin is an ", {b})
+
+elif user_2==c:
+    print("Your cabin is an ", {c})
+
+else:
+    print("Your cabin is " , {lux})
+
+
+#3
