13a42f5d796191b45050602d238b071a5f18ef8b
amberward423 <amberward423@gmail.com>
2025-11-02 23:45:32 +0200

    new commit

--- Mod 10 HW.py
+++ Mod 10 HW.py
@@ -4,6 +4,11 @@ class Elevator:
        self.t_floor_num = t_floor_num
 
 
+
+    def print_info(self):
+            super().print_info(b_floor_num=0,t_floor_num=5)
+
+
     def go_to_floor(self,new_floor,current_floor, end_floor1, end_floor2):
         self.end_floor1= end_floor1
         self.end_floor2= end_floor2
@@ -12,6 +17,7 @@ class Elevator:
         self.end_floor1 =+ current_floor + new_floor
         self.end_floor2 =+ current_floor - new_floor
         print(input("Enter floor number, please: "))
+        super().print_info(b_floor_num=0, t_floor_num=5)
 
     def floor_up(self,new_floor,current_floor):
         self.end_floor1 = self.end_floor1
@@ -20,6 +26,7 @@ class Elevator:
         print(input("Enter floor number, please: "))
         self.end_floor1= current_floor + new_floor
         print(f"You are now at floor {self.end_floor1}")
+        super().print_info(b_floor_num=0, t_floor_num=5)
 
     def floor_down(self,new_floor,current_floor, end_floor2):
         self.end_floor2 = end_floor2
@@ -28,6 +35,8 @@ class Elevator:
         print(input("Enter floor number, please: "))
         self.end_floor2 =+ current_floor - new_floor
         print(f"You are now at floor {self.end_floor2}")
+        super().print_info(b_floor_num=0, t_floor_num=5)
+
 
 class Building:
     def __init__(self,b_floor_num=0 , t_floor_num=5, e_num= 3):
@@ -35,10 +44,12 @@ class Building:
         self.t_floor_num = t_floor_num
         self.e_num= Elevator(e_num)
         self.elevator = []
+        super().print_info(b_floor_num=0, t_floor_num=5)
 
     def run_elevator (self,e_num, dest_floor):
         self.dest_floor= dest_floor
         self.e_num= e_num
+        super().print_info(b_floor_num=0, t_floor_num=5)
 
     def fire_alarm(self):
             for self.e_num in self.elevator:
@@ -46,6 +57,26 @@ class Building:
 
 
 
+building = Building()
+Building.run_elevator(0,0)
+Building.fire_alarm(Elevator)
+Elevator.floor_down(0,5,0,1)
+Elevator.floor_up(0,5,0)
+Elevator.go_to_floor(0,5,0,1)
 
 
 
+## in testing I got this error, and I am unsure of what to do moving forward.
+# I have sought help from friends, google, and geeksforgeeks and I am unable to figure this out
+# /Users/amberward/PycharmProjects/LearningPython/.venv/bin/python /Users/amberward/Documents/GitHub/LearningPython/Mod 10 HW.py
+#Traceback (most recent call last):
+  #File "/Users/amberward/Documents/GitHub/LearningPython/Mod 10 HW.py", line 69, in <module>
+    #building = Building()
+  #File "/Users/amberward/Documents/GitHub/LearningPython/Mod 10 HW.py", line 56, in __init__
+   # super().print_info(b_floor_num=0, t_floor_num=5)
+    #^^^^^^^^^^^^^^^^^^
+#AttributeError: 'super' object has no attribute 'print_info'
+
+##Process finished with exit code 1##
+
+## the 4th question is posted in mod 9
--- Mod 9 HW.py
+++ Mod 9 HW.py
@@ -38,7 +38,31 @@ class Car:
         self.max_speed = random.randrange(100, 200)
         return random.randrange(self.max_speed)
 
-## I am lost in terms of question 4, I do not understand how to program the race. I have worked on this for days and googled and youtubed, but I cannot solve this. Please advise.
+#I am completely lost in terms of question 4, I do not understand how to program the race. I have worked on this for a while and googled and you-tubed, but I cannot solve this.
+# Please advise.
+
+# The race class is from Mod 10
+class Race:
+    def __init__(self,name, kilometers):
+        self.name= name
+        self.kilometers= kilometers
+
+    def print_info(self):
+        pass
+
+    def hour_passes(self, hours, t_dist):
+        self.hours= hours
+        self.t_dist=t_dist
+        super(Car).print_info(hours,t_dist)
+## having trouble with the super statement
+
+
+
+
+
+
+
+
 cars=[]
 car1= Car("ABC-1",147,  0)
 car2= Car("ABC-2",147,  0)
